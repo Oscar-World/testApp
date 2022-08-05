6d695aed9eba5011e46348de3476bde704915a37
Oscar <rkdgjdl@naver.com>
2022-08-05 22:59:17 +0900

    test size of View

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,18 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate() 호출됨");
 
-        Button button = findViewById(R.id.testBtn);
-        TextView textView = findViewById(R.id.textView);
+//        Button button = findViewById(R.id.button);
+        TextView textView = (TextView) findViewById(R.id.textView);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clickNum += 1;
-                Log.d(TAG, "클릭 횟수 : " + clickNum);
-
-                textView.setText("터치 횟수 : " + String.valueOf(clickNum));
-            }
-        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                clickNum += 1;
+//                Log.d(TAG, "터치 횟수 : " + clickNum);
+//
+//                textView.setText("터치 횟수 : " + String.valueOf(clickNum));
+//            }
+//        });
 
     }
     @Override
