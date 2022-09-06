fefda2918b6d92cc7b972a798cc5693016904a56
Oscar <rkdgjdl@naver.com>
2022-09-06 20:57:44 +0900

    modify anim

--- app/src/main/java/com/example/myapplication/AnimActivity.java
+++ app/src/main/java/com/example/myapplication/AnimActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -16,13 +18,23 @@ public class AnimActivity extends AppCompatActivity {
 
         ImageView android = findViewById(R.id.android);
         Button button = findViewById(R.id.Button);
+        Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+        Animation scale = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.translate);
+        Animation alpha = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
+        Animation mix = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.mix);
+
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
+//                android.startAnimation(rotate);
+//                android.startAnimation(scale);
+//                android.startAnimation(translate);
+//                android.startAnimation(alpha);
+                android.startAnimation(mix);
 
             }
         });
