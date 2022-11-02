ad8e37cb2531ef75b5f19d2ef0bed8d30bad5045
Oscar <rkdgjdl@naver.com>
2022-11-02 20:07:41 +0900

    modify calculator

--- app/src/main/java/com/example/myapplication/Calculator.java
+++ app/src/main/java/com/example/myapplication/Calculator.java
@@ -3,19 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Calculator extends AppCompatActivity {
+
+    String TAG = "계산기";
 
     TextView result_Text, process_Text;
 
     Button no1_Btn, no2_Btn, no3_Btn, no4_Btn, no5_Btn, no6_Btn, no7_Btn, no8_Btn,
             no9_Btn, no0_Btn, plus_Btn, minus_Btn, division_Btn, multiplication_Btn,
             clear_Btn, erase_Btn, rest_Btn, point_Btn, change_Btn, calculate_Btn;
-
-    View.OnClickListener Click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,7 @@ public class Calculator extends AppCompatActivity {
 
         setView();
 
-        Click = new View.OnClickListener() {
+        View.OnClickListener Click = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -95,6 +100,27 @@ public class Calculator extends AppCompatActivity {
             }
         };
 
+        no1_Btn.setOnClickListener(Click);
+        no2_Btn.setOnClickListener(Click);
+        no3_Btn.setOnClickListener(Click);
+        no4_Btn.setOnClickListener(Click);
+        no5_Btn.setOnClickListener(Click);
+        no6_Btn.setOnClickListener(Click);
+        no7_Btn.setOnClickListener(Click);
+        no8_Btn.setOnClickListener(Click);
+        no9_Btn.setOnClickListener(Click);
+        no0_Btn.setOnClickListener(Click);
+        plus_Btn.setOnClickListener(Click);
+        minus_Btn.setOnClickListener(Click);
+        division_Btn.setOnClickListener(Click);
+        multiplication_Btn.setOnClickListener(Click);
+        rest_Btn.setOnClickListener(Click);
+        point_Btn.setOnClickListener(Click);
+        change_Btn.setOnClickListener(Click);
+        clear_Btn.setOnClickListener(Click);
+        erase_Btn.setOnClickListener(Click);
+        calculate_Btn.setOnClickListener(Click);
+
     }
 
     void addNumber(String str) {
@@ -135,53 +161,37 @@ public class Calculator extends AppCompatActivity {
 
     void result() {
 
+        String[] process = process_Text.getText().toString().split(" ");
+        Log.d(TAG, "data: " + Arrays.toString(process));
+
     }
 
     public void setView() {
 
         result_Text = findViewById(R.id.result_Text);
         process_Text = findViewById(R.id.process_Text);
-//        no1_Btn = findViewById(R.id.no1_Btn);
-//        no2_Btn = findViewById(R.id.no2_Btn);
-//        no3_Btn = findViewById(R.id.no3_Btn);
-//        no4_Btn = findViewById(R.id.no4_Btn);
-//        no5_Btn = findViewById(R.id.no5_Btn);
-//        no6_Btn = findViewById(R.id.no6_Btn);
-//        no7_Btn = findViewById(R.id.no7_Btn);
-//        no8_Btn = findViewById(R.id.no8_Btn);
-//        no9_Btn = findViewById(R.id.no9_Btn);
-//        no0_Btn = findViewById(R.id.no0_Btn);
-//        plus_Btn = findViewById(R.id.plus_Btn);
-//        minus_Btn = findViewById(R.id.minus_Btn);
-//        multiplication_Btn = findViewById(R.id.multiplication_Btn);
-//        division_Btn = findViewById(R.id.division_Btn);
-//        clear_Btn = findViewById(R.id.clear_Btn);
-//        erase_Btn = findViewById(R.id.erase_Btn);
-//        rest_Btn = findViewById(R.id.rest_Btn);
-//        point_Btn = findViewById(R.id.point_Btn);
-//        change_Btn = findViewById(R.id.change_Btn);
-//        calculate_Btn = findViewById(R.id.calculate_Btn);
+        no1_Btn = findViewById(R.id.no1_Btn);
+        no2_Btn = findViewById(R.id.no2_Btn);
+        no3_Btn = findViewById(R.id.no3_Btn);
+        no4_Btn = findViewById(R.id.no4_Btn);
+        no5_Btn = findViewById(R.id.no5_Btn);
+        no6_Btn = findViewById(R.id.no6_Btn);
+        no7_Btn = findViewById(R.id.no7_Btn);
+        no8_Btn = findViewById(R.id.no8_Btn);
+        no9_Btn = findViewById(R.id.no9_Btn);
+        no0_Btn = findViewById(R.id.no0_Btn);
+        plus_Btn = findViewById(R.id.plus_Btn);
+        minus_Btn = findViewById(R.id.minus_Btn);
+        multiplication_Btn = findViewById(R.id.multiplication_Btn);
+        division_Btn = findViewById(R.id.division_Btn);
+        clear_Btn = findViewById(R.id.clear_Btn);
+        erase_Btn = findViewById(R.id.erase_Btn);
+        rest_Btn = findViewById(R.id.rest_Btn);
+        point_Btn = findViewById(R.id.point_Btn);
+        change_Btn = findViewById(R.id.change_Btn);
+        calculate_Btn = findViewById(R.id.calculate_Btn);
 
-        no1_Btn.setOnClickListener(Click);
-        no2_Btn.setOnClickListener(Click);
-        no3_Btn.setOnClickListener(Click);
-        no4_Btn.setOnClickListener(Click);
-        no5_Btn.setOnClickListener(Click);
-        no6_Btn.setOnClickListener(Click);
-        no7_Btn.setOnClickListener(Click);
-        no8_Btn.setOnClickListener(Click);
-        no9_Btn.setOnClickListener(Click);
-        no0_Btn.setOnClickListener(Click);
-        plus_Btn.setOnClickListener(Click);
-        minus_Btn.setOnClickListener(Click);
-        division_Btn.setOnClickListener(Click);
-        multiplication_Btn.setOnClickListener(Click);
-        rest_Btn.setOnClickListener(Click);
-        point_Btn.setOnClickListener(Click);
-        change_Btn.setOnClickListener(Click);
-        clear_Btn.setOnClickListener(Click);
-        erase_Btn.setOnClickListener(Click);
-        calculate_Btn.setOnClickListener(Click);
+
 
 
 
